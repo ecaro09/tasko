7612e6db1af0db63d8b11dca4281db8abbe911e0
gabo09 <ecaro09@gmail.com>
2025-10-01 04:06:00 +0800

    Reverted all changes back to version c620f7ea462fb47bb08bf7e21b4bb054a16e5c1f

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import AppLayout from "./components/layout/AppLayout"; // Import the new AppLayout
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AppLayout> {/* Wrap the entire application with AppLayout */}
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/tasks/new" element={<Index />} /> {/* Temporary route for task creation */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </AppLayout>
-    </BrowserRouter>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
