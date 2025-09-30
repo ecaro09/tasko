c379cd97ab961a025f499183a42ef019c1c08cc4
gabo09 <ecaro09@gmail.com>
2025-10-01 05:46:38 +0800

    [dyad] Added a mobile bottom navigation bar - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -16,6 +16,7 @@ import { usePWA } from '@/hooks/use-pwa';
 import LoginModal from '@/components/LoginModal';
 import SignupModal from '@/components/SignupModal';
 import PostTaskModal from '@/components/PostTaskModal';
+import BottomNavigationBar from '@/components/BottomNavigationBar'; // Import the new component
 
 // Placeholder for task data - will be replaced with Firebase later
 const sampleTasks = [
@@ -147,7 +148,7 @@ const Index = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 pb-16 md:pb-0"> {/* Added pb-16 for bottom nav spacing */}
       {showSplashScreen && <SplashScreen />}
       <OfflineIndicator isVisible={!isOnline} />
 
@@ -155,7 +156,7 @@ const Index = () => {
         isAuthenticated={isAuthenticated}
         onSignIn={handleSignIn}
         onSignOut={handleSignOut}
-        onSignUp={handleSignUp} // Pass handleSignUp to Header
+        onSignUp={handleSignUp}
       />
       <HeroSection />
       <main className="container mx-auto p-4">
@@ -207,7 +208,7 @@ const Index = () => {
       </main>
       <AppFooter />
       <MadeWithDyad />
-      <Toaster /> {/* Add Toaster for sonner notifications */}
+      <Toaster />
 
       <LoginModal
         isOpen={showLoginModal}
@@ -232,6 +233,7 @@ const Index = () => {
         onInstall={installApp}
         onClose={closeInstallPrompt}
       />
+      <BottomNavigationBar /> {/* Add the BottomNavigationBar here */}
     </div>
   );
 };
