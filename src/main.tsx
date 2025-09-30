0d4ac8e4df383d8bfe77ed530ac4c13d30923921
gabo09 <ecaro09@gmail.com>
2025-10-01 05:48:17 +0800

    Reverted all changes back to version 071d4ee06affbfa91e18073c8a3fe3f3f77d1109

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { FirebaseProvider } from "./components/FirebaseProvider.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <FirebaseProvider>
-    <App />
-  </FirebaseProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
