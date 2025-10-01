09c8ef53886eb0acd9459b49add20ed79d3ce902
gabo09 <ecaro09@gmail.com>
2025-10-01 18:41:19 +0800

    [dyad] Removed unused performance optimizer file. - wrote 1 file(s), deleted 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import './performance-optimizer'; // Import the performance optimizer
 
 createRoot(document.getElementById("root")!).render(<App />);
