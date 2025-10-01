3537427a6304ce6604bf2fed0fccfc449850c5c8
gabo09 <ecaro09@gmail.com>
2025-10-01 18:04:26 +0800

    [dyad] Added performance optimizer script and imported it into main.tsx. - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import './performance-optimizer'; // Import the performance optimizer script
 
 createRoot(document.getElementById("root")!).render(<App />);
