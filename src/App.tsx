8023581c5f4ab5fa3a5552e4e41092c4e28def28
gabo09 <ecaro09@gmail.com>
2025-10-01 15:53:31 +0800

    Reverted all changes back to version f7567a445244ecd596ea9c11306e37e064e2cbf7

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { AuthProvider } from "./hooks/use-auth";
-import { ChatProvider } from "./hooks/use-chat";
-import ChatPage from "./pages/ChatPage";
-import PrivacyPolicy from "./pages/PrivacyPolicy"; // Import the new PrivacyPolicy component
+import { AuthProvider } from "./hooks/use-auth"; // Import AuthProvider
+import { ChatProvider } from "./hooks/use-chat"; // Import ChatProvider
+import ChatPage from "./pages/ChatPage"; // Import ChatPage
 
 const queryClient = new QueryClient();
 
@@ -14,12 +13,11 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <BrowserRouter>
-        <AuthProvider>
-          <ChatProvider>
+        <AuthProvider> {/* Wrap with AuthProvider */}
+          <ChatProvider> {/* Wrap with ChatProvider */}
             <Routes>
               <Route path="/" element={<Index />} />
-              <Route path="/chat" element={<ChatPage />} />
-              <Route path="/privacy" element={<PrivacyPolicy />} /> {/* Add PrivacyPolicy route */}
+              <Route path="/chat" element={<ChatPage />} /> {/* Add ChatPage route */}
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
