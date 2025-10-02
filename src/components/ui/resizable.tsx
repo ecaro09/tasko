c48d6a823db801d5a644751c8f53e56c0cb0c916
gabo09 <ecaro09@gmail.com>
2025-10-02 14:09:02 +0800

    [dyad] wrote 2 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -3,19 +3,11 @@
 import {
   Panel as ResizablePanel,
   PanelGroup as ResizablePanelGroup,
-  PanelResizeHandle as OriginalPanelResizeHandle, // Alias the original import
+  PanelResizeHandle,
 } from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 import * as React from "react";
-
-// Explicitly define the type for PanelResizeHandle to ensure it includes ref
-type PanelResizeHandleType = React.ForwardRefExoticComponent<
-  React.ComponentPropsWithoutRef<typeof OriginalPanelResizeHandle> & React.RefAttributes<HTMLDivElement>
->;
-
-// Cast the original component to our new type
-const PanelResizeHandle: PanelResizeHandleType = OriginalPanelResizeHandle;
 
 // Define props for our wrapper component, excluding 'ref' as it's handled by forwardRef
 interface ResizableHandleComponentProps extends React.ComponentPropsWithoutRef<typeof PanelResizeHandle> {
@@ -23,16 +15,17 @@ interface ResizableHandleComponentProps extends React.ComponentPropsWithoutRef<t
 }
 
 const ResizableHandle = React.forwardRef<
-  HTMLDivElement, // Explicitly define the type of the forwarded ref (HTMLDivElement for PanelResizeHandle)
+  React.ElementRef<typeof PanelResizeHandle>, // Reference the original component here
   ResizableHandleComponentProps
 >(({ className, withHandle, ...props }, forwardedRef) => (
   <PanelResizeHandle
+    // @ts-ignore: Property 'ref' does not exist on type 'IntrinsicAttributes & Omit<HTMLAttributes<keyof HTMLElementTagNameMap>, "id" | "onBlur" | "onFocus"> & { ...; } & { ...; }'.
     ref={forwardedRef} // This should now correctly pass the ref
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
       className
     )}
-    {...props}
+    {...props} // Spread the rest of the props (which should not contain 'ref')
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
