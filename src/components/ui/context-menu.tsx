0a1f8ac915bddef1d9dd82eaed01fa0f85dfd9a2
gabo09 <ecaro09@gmail.com>
2025-10-02 12:41:09 +0800

    [dyad] wrote 6 file(s)

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -52,15 +52,10 @@ const ContextMenuSubContent = React.forwardRef<
 ));
 ContextMenuSubContent.displayName = ContextMenuPrimitive.SubContent.displayName;
 
-// Explicitly define ContextMenuContentProps to include sideOffset
-interface ContextMenuContentProps extends React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.Content> {
-  sideOffset?: number;
-}
-
 const ContextMenuContent = React.forwardRef<
   React.ElementRef<typeof ContextMenuPrimitive.Content>,
-  ContextMenuContentProps // Use the explicit interface here
->(({ className, sideOffset = 4, ...props }, ref) => ( // Destructure sideOffset here
+  React.ComponentPropsWithRef<typeof ContextMenuPrimitive.Content> // Use ComponentPropsWithRef directly
+>(({ className, sideOffset = 4, ...props }, ref) => (
   <ContextMenuPrimitive.Content
     ref={ref}
     sideOffset={sideOffset} // Pass the destructured sideOffset directly
@@ -69,7 +64,7 @@ const ContextMenuContent = React.forwardRef<
       "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
       className
     )}
-    {...props} // Spread the rest of the props
+    {...props}
   />
 ));
 ContextMenuContent.displayName = ContextMenuPrimitive.Content.displayName;
