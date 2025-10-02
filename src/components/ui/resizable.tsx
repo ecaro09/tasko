47e1a4fb294de1c0964507ebc03f517d9669d50a
gabo09 <ecaro09@gmail.com>
2025-10-02 13:34:32 +0800

    [dyad] wrote 3 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -3,27 +3,28 @@
 import {
   Panel as ResizablePanel,
   PanelGroup as ResizablePanelGroup,
-  PanelResizeHandle as ResizablePanelHandle,
+  PanelResizeHandle, // Import the original component directly
 } from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 import * as React from "react";
 
-type ResizableHandleProps = React.ComponentPropsWithoutRef<typeof ResizablePanelHandle> & {
+// Use ComponentPropsWithoutRef as 'ref' is passed separately by forwardRef
+type ResizableHandleProps = React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
   withHandle?: boolean; // Custom prop
 };
 
 const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof ResizablePanelHandle>,
+  React.ElementRef<typeof PanelResizeHandle>, // Reference the original component here
   ResizableHandleProps
->(({ className, withHandle, ...props }, ref) => (
-  <ResizablePanelHandle
-    ref={ref} // Pass the ref directly to the underlying component
+>(({ className, withHandle, ...props }, forwardedRef) => (
+  <PanelResizeHandle // Use the original component here
+    ref={forwardedRef} // This should now correctly pass the ref
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
       className
     )}
-    {...props} // Spread the rest of the props
+    {...props} // Spread the rest of the props (which should not contain 'ref')
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
